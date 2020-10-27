1a317ba0ddf792cb8a456aca4c4d651a4da5826f
Puujee <puujee@Puujees-MacBook-Pro.local>
2020-10-27 18:38:04 -0500

    deploy service

--- src/main/java/com/pm/ecommerce/order_service/OrderServiceApplication.java
+++ src/main/java/com/pm/ecommerce/order_service/OrderServiceApplication.java
@@ -3,10 +3,12 @@ package com.pm.ecommerce.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.pm.ecommerce.entities"})
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/pm/ecommerce/order_service/services/impl/CartItemService.java
+++ src/main/java/com/pm/ecommerce/order_service/services/impl/CartItemService.java
@@ -2,7 +2,9 @@ package com.pm.ecommerce.order_service.services.impl;
 
 import com.pm.ecommerce.entities.CartItem;
 import com.pm.ecommerce.order_service.services.ICartItemService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartItemService implements ICartItemService {
 
     @Override
