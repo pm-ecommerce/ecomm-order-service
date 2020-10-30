de310920b46510fc2f340a263e65a172a5cf4cf1
Sandeep Giri <ioesandeep@gmail.com>
2020-10-30 17:28:46 -0400

    Configure for build

--- src/main/java/com/pm/ecommerce/order_service/OrderServiceApplication.java
+++ src/main/java/com/pm/ecommerce/order_service/OrderServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.pm.ecommerce.entities"})
